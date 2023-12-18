23dad7dac0250d4e81d700b016f4ae39b27c1322
unknown <linhd@cddk.cn>
2023-12-18 20:52:34 +0800

    update

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/0xlinxiuzhu",
+  title: "林修竹",
+  description: "",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '小小的我', link: 'https://0xlinxiuzhu.github.io/collection/' },
+      //{ text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    //sidebar: [
+    //  {
+    //    text: 'Examples',
+    //    items: [
+    //      { text: 'Markdown Examples', link: '/markdown-examples' },
+    //      { text: 'Runtime API Examples', link: '/api-examples' }
+    //    ]
+    //  }
+    //],
+
+    //socialLinks: [
+    //  { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    //]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "林修竹"
+  text: ""
+  tagline: 三年二班
+  #actions:
+  #  - theme: brand
+  #    text: Markdown Examples
+  #    link: /markdown-examples
+  #  - theme: alt
+  #    text: API Examples
+  #    link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
