b300cafefa5de44d49b8721cad91897173f9689f
0xlinxiuzhu <linxiuzhu.eth@gmail.com>
2024-06-30 19:03:35 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/0xlinxiuzhu",
-  title: " ",
+  title: "林修竹",
   description: "",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
